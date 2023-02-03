d5cb2089ff6775de88e946f0ccd0931e52e22594
Aleksandr Dalbezh <dalbezh@icloud.com>
2023-02-03 17:53:23 +0300

    add items.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+from items import store_items, shop_items
+
 
 class Storage(ABC):
     @abstractmethod
@@ -31,84 +33,130 @@ class Store(Storage):
         self._items = items
         self._capacity = capacity
 
-    def add(self, name, number):
-        pass
+    def add(self, name, amount):
+        if self.get_free_space() < amount:
+            raise NotEnouthSpace
 
-    def remove(self, name, number):
-        pass
+        if name in self._items:
+            self._items[name] += amount
+
+        else:
+            self._items[name] = amount
+
+
+    def remove(self, name, amount):
+        if name not in self._items or self._items[name] < amount: # вот это надо разобрать !!!
+            raise NotEnouthSpace
+
+        self._items[name] -= amount
+
+        if self._items[name] == 0:
+            self._items.pop(name)
+
 
     def get_free_space(self):
-        pass
+        """
+        Считает свободное место
+        """
+        return self._capacity - sum(self._items.values())
 
     def get_items(self):
-        pass
+        items = [f"{item} - {amount}" for item, amount in self._items.items()]
+        return items
 
     def get_unique_items_count(self):
-        pass
+        return len(self._items)
 
 
 class Shop(Storage):
     """
     Магазин
     """
-    def __init__(self, items, capacity=20):
+    def __init__(self, items, capacity=100):
         self._items = items
         self._capacity = capacity
 
-    def add(self, name, number):
-        pass
+    def add(self, name, amount):
+        if self.get_unique_items_count() >= 5: # проверка, что в магазине храниться меньше 5 уникальных товаров.
+            raise ToManyDiffProd
 
-    def remove(self, name, number):
-        pass
+        if self.get_free_space() < amount:
+            raise NotEnouthSpace
+
+        if name in self._items:
+            self._items[name] += amount
+
+        else:
+            self._items[name] = amount
+
+
+    def remove(self, name, amount):
+        if name not in self._items or self._items[name] < amount: # вот это надо разобрать !!!
+            raise NotEnouthSpace
+
+        self._items[name] -= amount
+
+        if self._items[name] == 0:
+            self._items.pop(name)
+
 
     def get_free_space(self):
-        pass
+        """
+        Считает свободное место
+        """
+        return self._capacity - sum(self._items.values())
 
     def get_items(self):
-        pass
+        items = [f"{item} - {amount}" for item, amount in self._items.items()]
+        return items
 
     def get_unique_items_count(self):
-        pass
+        return len(self._items)
 
 
 class Request:
-    from_ = ""
-    to = ""
-    amount = 3
-    product = "печеньки"
+    def __init__(self, request: str, storages):
+        """
+        Обработка запроса пользоватля, который должeн выглядеть так:
+        Доставить 3 печеньки из склад в магазин
+        """
+        user_request = request.lower().split(" ")
+        if len(user_request) != 7 and "доставить" not in user_request:
+            raise BadRequest
 
-    @classmethod
-    def set_from(cls, from_: str):
-        cls.from_ = from_
-        return cls.from_
+        self.amount = int(user_request[1])
+        self.product = user_request[2]
+        self.from_ = user_request[4]
+        self.to = user_request[6]
 
-    @classmethod
-    def set_to(cls, to: str):
-        cls.to = to
-        return cls.to
+        if self.from_ not in storages or self.to not in storages: # а оно тебе надо браток
+            raise BadStorageName
 
-    @classmethod
-    def set_amount(cls, amount: int):
-        cls.amount = amount
-        return cls.amount
-
-    @classmethod
-    def set_product(cls, product: str):
-        cls.product = product
-        return cls.product
 
 
 
 def main():
-    store = Store('test')
-    print(store)
+    store = Store(store_items)
+    shop = Shop(shop_items)
 
-    test_from = Request.set_from("склад")
-    test_to = Request.set_to("магазин")
+    storages = {
+        "склад": store,
+        "магазин": shop
+    }
 
-    print(f"{Request.to}, {Request.from_}, {Request.amount}, {Request.product}")
+
+    for _ in range(1): 
+        for storage_name in storages:
+            print(f"В {storage_name} храниться:\n")
+            for item in storages[storage_name].get_items(): 
+                print(item)
+            print("\n")
+
+        user_input = input("Ваш запрос: ")
+
+        request = Request(request=user_input, storages=storages)
+        # Доставить 3 печеньки из склад в магазин
 
 
 if __name__ == '__main__':
     main()
-    
